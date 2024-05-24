e52068a7f8f864f052c37c6c23786262a7f9206d
David Plankensteiner <da.plankensteiner@gmail.com>
2024-05-24 13:27:36 +0200

    Update build script using rust image

--- build_for_raspberry.sh
+++ build_for_raspberry.sh
@@ -1,2 +1,11 @@
-export PKG_CONFIG_SYSROOT_DIR=/usr/arm-linux-gnueabihf/
-cargo build --release --target=armv7-unknown-linux-gnueabihf
+#export PKG_CONFIG_SYSROOT_DIR=/usr/arm-linux-gnueabihf/
+#cargo build --release --target=armv7-unknown-linux-gnueabihf
+
+# docker run --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/sunny -w /usr/src/sunny rust:bullseye \
+#     /bin/bash -c \
+#     "rustup target add armv7-unknown-linux-gnueabihf;
+#     sudo apt install arm-linux-gnueabihf-gcc;
+#     cargo build --release --target=armv7-unknown-linux-gnueabihf"
+
+docker build -f Dockerfile-build -t sunny-build-raspberry . && \
+docker run --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/sunny -w /usr/src/sunny sunny-build-raspberry cargo build --release --target=armv7-unknown-linux-gnueabihf
