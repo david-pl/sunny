3a39b4a383538ba063b155eed55fb31b90198d75
David Plankensteiner <da.plankensteiner@gmail.com>
2024-05-24 15:00:33 +0200

    More unit tests for statistics

--- sunny_db/src/statistics.rs
+++ sunny_db/src/statistics.rs
@@ -46,15 +46,6 @@ pub trait MinMaxOfSeries<T> {
     fn max_by<F>(&self, f: F) -> Option<T>
         where
             F: FnMut(&T, &T) -> Ordering;
-    // fn max(&self) -> Option<T>;
-    // fn max_by_key<F>(&self, f: F) -> Option<T>
-    // where
-    //     F: FnMut(&T) -> T;
-
-    // fn min(&self) -> Option<T>;
-    // fn min_by_key<F>(&self, f: F) -> Option<T>
-    // where
-    //     F: FnMut(&T) -> T;
 }
 
 impl<T> MinMaxOfSeries<T> for TimeSeries<T>
@@ -72,33 +63,8 @@ where
         where
             F: FnMut(&T, &T) -> Ordering {
         let values = self.get_current_values_without_time();
-        values.into_iter().min_by(f)
+        values.into_iter().max_by(f)
     }
-    // fn max(&self) -> Option<T> {
-    //     let values = self.get_current_values_without_time();
-    //     values.into_iter().max()
-    // }
-
-    // fn max_by_key<F>(&self, f: F) -> Option<T>
-    // where
-    //     F: FnMut(&T) -> T,
-    // {
-    //     let values = self.get_current_values_without_time();
-    //     values.into_iter().max_by_key(f)
-    // }
-
-    // fn min(&self) -> Option<T> {
-    //     let values = self.get_current_values_without_time();
-    //     values.into_iter().min()
-    // }
-
-    // fn min_by_key<F>(&self, f: F) -> Option<T>
-    // where
-    //     F: FnMut(&T) -> T,
-    // {
-    //     let values = self.get_current_values_without_time();
-    //     values.into_iter().min_by_key(f)
-    // }
 }
 
 pub trait Average<T> {
@@ -151,20 +117,76 @@ mod tests {
 
     #[test]
     fn test_integral() -> () {
-        let mut ts = TimeSeries::<f64>::new(10);
         let times: Vec<u64> = vec![0, 10, 20, 30, 40];
 
+        // simplest case: linear with slope 1
+        let mut ts = TimeSeries::<f64>::new(10);
         for i in 0..times.len() {
             ts.insert_value_at_time(times[i], i as f64);
         }
-
         let integral = ts.integrate().unwrap();
-
         assert_eq!(integral, 80.0);
 
-        let m = ts.max_by(|a, b| b.partial_cmp(a).unwrap()).unwrap();
-
+        let m = ts.max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
         assert_eq!(m, (times.len() - 1) as f64);
+
+        // linear with slope != 1
+        let times: Vec<u64> = (0..100).collect();
+        let mut ts = TimeSeries::<f64>::new(times.len());
+        let k = 0.23;
+        for i in 0..times.len() {
+            ts.insert_value_at_time(times[i], k * times[i] as f64);
+        }
+
+        let integral = ts.integrate().unwrap();
+        let d = integral - (ts.get_current_values().last().unwrap().1 * times[times.len() - 1] as f64) / 2.0;
+        let d_abs = if d < 0.0 {
+            -d
+        } else {
+            d
+        };
+        assert!(d_abs < 0.0001);
+
+
+        // non-linear case
+        fn f_nl(x: f64) -> f64 {
+            2.0*x - x*x
+        }
+        fn f_nl_integrated(x: f64) -> f64 {
+            x*x - x*x*x / 3.0
+        }
+
+        let mut ts = TimeSeries::<f64>::new(times.len());
+        for i in 0..times.len() {
+            ts.insert_value_at_time(times[i], f_nl(times[i] as f64));
+        }
+
+        let integral = ts.integrate().unwrap();
+        let definite_integral = f_nl_integrated(times[times.len() - 1] as f64) - f_nl_integrated(times[0] as f64);
+        
+        // difference, but normalized to the total value since that's pretty large and the approximation may not be that good
+        let d = (integral - definite_integral) / integral;
+        let d_abs = if d < 0.0 {
+            -d
+        } else {
+            d
+        };
+        assert!(d_abs < 0.0001);
+
+        assert_eq!(ts.max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap(), 1.0);
+
+        assert_eq!(ts.min_by(|a, b| a.partial_cmp(b).unwrap()).unwrap(), ts.get_current_values().last().unwrap().1);
+
+        // test the average
+        let avg1 = ts.average().unwrap();
+        let avg2 = integral / (times[times.len() - 1] - times[0]) as f64;
+        let d = avg1 - avg2;
+        let d_abs = if d < 0.0 {
+            -d
+        } else {
+            d
+        };
+        assert!(d_abs < 0.0001);
     }
 
 }
